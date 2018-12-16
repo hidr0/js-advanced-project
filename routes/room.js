d0171678f9462422fa3daceb3e0004271a728455
Dimitar Lukanov <dimiturlukanov@gmail.com>
2018-12-16 17:33:05 +0200

    Added mustache as the render engine and added rendering to the '/' route

--- routes/room.js
+++ routes/room.js
@@ -2,8 +2,12 @@ const Router = require('express').Router;
 
 const roomRouter = Router();
 
+roomRouter.post('/', (request, response) => {
+  
+});
+
 roomRouter.get('/', (request, response) => {
-  response.send("Should have a table containing the rooms and a button to join the specific room");
+  response.render("Should have a table containing the rooms and a button to join the specific room");
 })
 
 module.exports.roomRouter = roomRouter;
--- routes/startmenu.js
+++ routes/startmenu.js
@@ -3,7 +3,11 @@ const Router = require('express').Router;
 const startMenuRouter = Router();
 
 startMenuRouter.get('/',(request, response)=> {
-  response.send("Should contain a username field and a continue button");
+  response.render("startmenu/index");
+});
+
+startMenuRouter.post('/', (req, res)=> {
+  res.send(req.body["name"])
 });
 
 
