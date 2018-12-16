8cf3d689c6eb743e49c5e18ffeeee143dd715d33
Mihail Kirilov <hidr0.frbg@gmail.com>
2018-12-16 17:26:37 +0200

    Added sockets to open and to display the userNames of the people entering the rooms.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 const roomRouter = require('./routes/room').roomRouter;
 const lbRouter = require('./routes/leaderboard').lbRouter;
@@ -10,4 +10,43 @@ app.use('/', startmenuRouter);
 app.use('/rooms', roomRouter);
 app.use('/leaderboard', lbRouter);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/', function (req, res) {
+    res.send("Index")
+});
+
+let rooms = {};
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function (socket) {
+        console.log('user disconnected');
+    });
+
+    socket.on('enteringRoom', function (params) {
+        roomName = params['roomName'];
+        userName = params['userName'];
+        newJoinedUser = {
+            userName: userName,
+            socket: socket
+        };
+
+        if (rooms[roomName] === undefined) {
+            rooms[roomName] = [newJoinedUser];
+        } else {
+            rooms[roomName].push(newJoinedUser);
+        }
+        console.log(rooms);
+
+        rooms[roomName].forEach((user) => {
+            user['socket'].emit('userJoined', {
+                userName: newJoinedUser['userName']
+            })
+        })
+
+
+    })
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
--- routes/room.js
+++ routes/room.js
@@ -1,9 +1,13 @@
 const Router = require('express').Router;
-
+const path = require('path')
 const roomRouter = Router();
 
 roomRouter.get('/', (request, response) => {
   response.send("Should have a table containing the rooms and a button to join the specific room");
+})
+
+roomRouter.get('/:id', (request, res) => {
+  res.sendFile(path.join(__dirname, '../', 'views/rooms/show.html'));
 })
 
 module.exports.roomRouter = roomRouter;
